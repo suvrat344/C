7f2abc218e3590b8ede39bb3e178fbc74ad540b2
Suvrat344 <chauhansuvrat344@gmail.com>
2024-06-02 23:32:41 +0530

    Instruction Expression And Operator Question

---
+++ 12 InstructionExpressionAndOperator/p18.c
@@ -0,0 +1,11 @@
+// Create a program to convert Fahrenheit to Celsius
+//  °C = (°F - 32) × 5/9
+
+#include <stdio.h>
+
+int main(){
+  float fahrenheit = -40;
+  printf("Temperatute in celcius : %.2f",(fahrenheit - 32)* 5 /9);
+
+  return 0;
+}
