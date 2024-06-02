f265a279b71fd33427ec186bb0eb399556cb72a8
Suvrat344 <chauhansuvrat344@gmail.com>
2024-06-02 23:31:26 +0530

    Fundamental Question

---
+++ 11 Fundamental/p12.c
@@ -0,0 +1,23 @@
+// Show the following patterns using single print statement:
+/*
+*                   *****                       *
+**                   ****                      **
+***                   ***                     ***
+****                   **                    ****
+*****                   *                   ***** 
+*/
+
+#include <stdio.h>
+
+int main(){
+  printf("First Pattern\n");
+  printf("*\n**\n***\n****\n*****\n");
+
+  printf("Second Pattern Pattern\n");
+  printf("******\n****\n***\n**\n*\n");
+
+  printf("Third Pattern\n");
+  printf("    *\n   **\n  ***\n ****\n*****\n");
+
+  return 0;
+}
---
+++ 11 Fundamental/p18.c
@@ -0,0 +1,12 @@
+// Create a program to define a constant for the mathematical value pi (3.14159) and use it to calculate and print the circumference of a circle with a radius input from use
+
+#include <stdio.h>
+#define PI 3.14159
+
+int main(){
+  float radius = 54;
+  printf("Circumference of Circle : %f",2*PI*radius);
+
+  return 0;
+}
+
