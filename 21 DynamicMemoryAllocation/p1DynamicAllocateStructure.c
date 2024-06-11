1a650f262a554c96822a59f9196b502de7a4d904
Suvrat344 <chauhansuvrat344@gmail.com>
2024-06-11 23:26:14 +0530

    Dynamic Memory Allocation Question

---
+++ 21 DynamicMemoryAllocation/p1DynamicAllocateStructure.c
@@ -0,0 +1,66 @@
+// Create a program that dynamically allocates memory for a Car structure and then free it at the end of the program
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct 
+{
+  char make[50];
+  char model[50];
+  char year[6];
+  char color[25];
+} Car;
+
+
+void input_car(Car *Cars,int size){
+  for (int i = 0; i < size; i++)
+  {
+    printf("Enter name of company : ");
+    scanf("%s",(Cars+i)->make);
+    printf("Enter model of car : ");
+    scanf("%s",(Cars+i)->model);
+    printf("Enter year of manufacturing : ");
+    scanf("%s",(Cars+i)->year);
+    printf("Enter color of car : ");
+    scanf("%s",(Cars+i)->color);
+    printf("---------**********------------\n");
+  }
+}
+
+
+void display(Car *Cars,int size){
+  for (int i = 0; i < size; i++)
+  {
+    printf("Name of Company : %s\n",(Cars+i)->make);
+    printf("Model of Car : %s\n",(Cars+i)->model);
+    printf("Manufacturing Year : %s\n",(Cars+i)->year);
+    printf("Color : %s\n",(Cars+i)->color);
+    printf("-------**********--------------\n");
+  }
+}
+
+
+int main(){
+  int n;
+
+  printf("Enter no of car you want to enter : ");
+  scanf("%d",&n);
+
+  Car *Cars = (Car *)malloc(n*sizeof(Car));
+  if (Cars == NULL)
+  {
+    printf("Memory not allocated");
+  }
+  else
+  {
+    input_car(Cars,n);
+  }
+
+  display(Cars,n);
+
+  free(Cars);
+  Cars = NULL;
+
+  return 0;
+}
