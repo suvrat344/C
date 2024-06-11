87d934f6c77855b2169355a40fdebe3e27fa198a
Suvrat344 <chauhansuvrat344@gmail.com>
2024-06-11 17:01:51 +0530

    Instructor Expression And Operator

---
+++ 12 InstructionExpressionAndOperator/p12Conversion.c
@@ -0,0 +1,11 @@
+// Given an integer value, convert it to a floating-point value and print both.
+
+#include <stdio.h>
+
+int main(){
+  int a=7;
+  printf("Integer Value : %d\n",a);
+  printf("Floating Value : %f\n",(float) a);
+
+  return 0;
+}
---
+++ 12 InstructionExpressionAndOperator/p14PerimeterOfRectangle.c
@@ -0,0 +1,11 @@
+// .Create a program to calculate Perimeter of a rectangle. 
+// Perimeter of rectangle ABCD = A+B+C+D
+
+#include <stdio.h>
+
+int main(){
+  int length = 25,breadth = 35;
+  printf("Perimeter of rectangle = %d",2*(length + breadth));
+
+  return 0;
+}
