e02ef56e443d5a0adca52aa4dab9df544139b120
Suvrat344 <chauhansuvrat344@gmail.com>
2024-06-12 00:00:50 +0530

    File Handling Question

---
+++ 22 FileHandling/p2ReadFile.c
@@ -0,0 +1,25 @@
+// Create a program that reads integers from a file and calculates their sum
+
+#include <stdio.h>
+
+int main(){
+  FILE *fptr = fopen("sum.txt","r");
+
+  if(fptr == NULL){
+    printf("Operation Failed");
+    return 1;
+  }
+
+  int sum=0,num1;
+
+  while(fscanf(fptr,"%d",&num1) == 1){
+    sum = sum + num1;
+  }
+
+  printf("%d",sum);
+
+  fclose(fptr);
+  fptr = NULL;
+
+  return 0;
+}
